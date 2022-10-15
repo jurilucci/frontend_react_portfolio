14df0ca990c97b9fb8c1d03445881c0e80096e63
jurilucci <juri.lucci@geoin.it>
2022-10-15 12:00:28 +0200

    change image in Header.jsx

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -58,7 +58,7 @@ const Header = () => (
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
     >
-      {[images.javascript, images.angular, images.react].map((circle, index) => (
+      {[images.javascript, images.angular, images.vue].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
           <img src={circle} alt="profile_bg" loading="lazy" />
         </div>
